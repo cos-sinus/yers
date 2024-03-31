36741194e0d9e5040a03c8641458d49a120d14ea
cos-sinus <As10819i@yandex.ru>
2024-03-31 21:24:03 +0300

    HW4-1

---
+++ HW4-1/OneDimenshionalArr.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Drawing;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace HW3_3
+{
+    public class OneDimenshionalArr<T>
+    {
+        public T[] arr;
+        private int _size;
+        private int _capacity;
+        private Random rnd = new Random();
+
+        public OneDimenshionalArr(int capacity)
+        {
+            _capacity = capacity;
+            T[] arr = new T[_capacity];
+            _size = 0;
+        }
+
+        public OneDimenshionalArr() : this(7)
+        {
+        }
+
+        public void AddElem(T elem)
+        {
+            if (_size >= _capacity)
+            {
+                _capacity = 2 * _capacity + 1;
+                T[] mass = new T[_capacity];
+                Array.Copy(arr, mass, _size);
+                arr = mass;
+            }
+            arr[_size] = elem;
+            _size++;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Array:");
+            for (int i = 0; i < _size; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+            Console.WriteLine(" ");
+        }
+
+        public void DelElem(int index)
+        {
+            if (index <= _size)
+            {
+                T[] array = new T[_capacity];
+                for (int i = 0; i < _size; i++)
+                {
+                    if (i != index)
+                    {
+                        array[i] = arr[i];
+                    }
+                }
+                _size--;
+                arr = array;
+            }
+            else
+            {
+                Console.WriteLine("Index is out of range!");
+            }
+        }
+
+        public void SortArr()
+        {
+            Array.Sort(arr, 0, _size);
+        }
+
+        public int CountElem()
+        {
+            return _size;
+        }
+
+        public int CountSomeElem(Func<T, bool> action)
+        {
+            int counter = 0;
+            for (int i = 0; i < _size; i++)
+            {
+                if (action(arr[i]))
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        public bool IfElem(Func<T, bool> action)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                if (action(arr[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool IfAllElems(Func<T, bool> action)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                if (!action(arr[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool IsElem(T elem)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                if (arr.Contains(elem))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void FirstIfElem(Func<T, bool> action)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                if (action(arr[i]))
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+            Console.WriteLine("Index is out of range!");
+        }
+
+        public void ActionOnArr(Func<T, T> action)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                Console.WriteLine(action(arr[i]));
+            }
+        }
+
+        public void ElemsActionOnArr(Func<T, bool> action)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                if (action(arr[i]))
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+        }
+
+        public void Rra()
+        {
+            Array.Reverse(arr, 0, _size);
+        }
+
+        public void MinMaxElem()
+        {
+            Array.Sort(arr, 0, _size);
+            T min = arr[0];
+            T max = arr[_size];
+            Console.WriteLine("Minimum: " + min);
+            Console.WriteLine("Maximum: " + max);
+        }
+
+        public void MinMaxElemByProection(Func<T, T> action)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                action(arr[i]);
+            }
+            Array.Sort(arr, 0, _size);
+            T min = arr[0];
+            T max = arr[_size];
+            Console.WriteLine("Minimum: " + min);
+            Console.WriteLine("Maximum: " + max);
+        }
+
+        public void ProjElemToNewType(Func<T, string> action)
+        {
+            arr.Select(action).ToArray();
+        }
+
+        public void GiveDiaposoneElems(int numofelems, int index)
+        {
+            for (int i = index; i < index + numofelems; i++)
+            {
+                Console.WriteLine(arr[i]);
+                Console.WriteLine(" ");
+            }
+        }
+
+        public void Iteration()
+        {
+            foreach (var elem in arr)
+            {
+                Console.Write(elem + " ");
+            }
+        }
+    }
+}
---
+++ HW4-1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace HW3_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            OneDimenshionalArr<int> intArr = new();
+            OneDimenshionalArr<double> doubleArr = new();
+
+            intArr.AddElem(10);
+            intArr.AddElem(20);
+            intArr.AddElem(30);
+            intArr.AddElem(40);
+            intArr.AddElem(50);
+            intArr.Print();
+            doubleArr.AddElem(2.3);
+            doubleArr.AddElem(3.3);
+            doubleArr.AddElem(4.3);
+            doubleArr.AddElem(5.3);
+            doubleArr.AddElem(6.3);
+            doubleArr.Print();
+
+            intArr.DelElem(4);
+            intArr.Print();
+            doubleArr.DelElem(2);
+            doubleArr.Print();
+
+            intArr.SortArr();
+            intArr.Print();
+            doubleArr.SortArr();
+            doubleArr.Print();
+
+            intArr.CountElem();
+            intArr.Print();
+            doubleArr.CountElem();
+            doubleArr.Print();
+
+            Console.WriteLine(intArr.CountSomeElem(x => Math.Abs(x) < 100));
+            Console.WriteLine(doubleArr.CountSomeElem(x => Math.Abs(x) < 100));
+
+            Console.WriteLine(intArr.IfElem(x => Math.Abs(x) < 100));
+            Console.WriteLine(doubleArr.IfElem(x => Math.Abs(x) < 100));
+
+            Console.WriteLine(intArr.IfAllElems(x => Math.Abs(x) < 100));
+            Console.WriteLine(doubleArr.IfAllElems(x => Math.Abs(x) < 100));
+
+            Console.WriteLine(intArr.IsElem(int.Parse(Console.ReadLine())));
+            Console.WriteLine(doubleArr.IsElem(double.Parse(Console.ReadLine())));
+
+            intArr.FirstIfElem((x) => Math.Abs(x) < 100);
+            doubleArr.FirstIfElem(x => Math.Abs(x) < 100);
+
+            int a = int.Parse(Console.ReadLine());
+            intArr.ActionOnArr((x) => x * 2);
+            doubleArr.ActionOnArr((x) => x * 2);
+
+            intArr.ElemsActionOnArr(x => x % 2 == 0);
+            doubleArr.ElemsActionOnArr(x => x % 2 == 0);
+
+            intArr.Rra();
+            intArr.Print();
+            doubleArr.Rra();
+            doubleArr.Print();
+
+            intArr.MinMaxElem();
+            intArr.Print();
+            doubleArr.MinMaxElem();
+            doubleArr.Print();
+
+            intArr.MinMaxElemByProection((x) => x * x);
+            doubleArr.MinMaxElemByProection((x) => x * x);
+
+            intArr.ProjElemToNewType((x) => x.ToString());
+            intArr.Print();
+            doubleArr.ProjElemToNewType((x) => x.ToString());
+            doubleArr.Print();
+
+            intArr.GiveDiaposoneElems(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+            doubleArr.GiveDiaposoneElems(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+
+            intArr.Iteration();
+            doubleArr.Iteration();
+        }
+    }
+}
